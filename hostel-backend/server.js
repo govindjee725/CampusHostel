d2cbcc1055b4423266c95dacc63952bdfb86aee5
govindjee725 <govindjee725@gmail.com>
2026-08-22 23:18:13 +0530

    fix

--- hostel-backend/server.js
+++ hostel-backend/server.js
@@ -19,8 +19,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true
+   origin: [
+      "http://localhost:5173",
+      "https://campus-hostel.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
   })
 );
 
